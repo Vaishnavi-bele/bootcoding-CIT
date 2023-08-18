2fd2cf183d3cd0965e972026a1fae83f8ec5ad64
Vaishnavi-bele <142570287+Vaishnavi-bele@users.noreply.github.com>
2023-08-18 15:39:28 +0530

    Add files via upload
    condition program

---
+++ Condition.java
@@ -0,0 +1,12 @@
+
+public class Condition{
+public static void main(String[] arg){
+int number; //declaration
+number=29;//initialisation
+if(number%2==0){
+System.out.print(number+"is even number");
+}else{
+System.out.print(number+"is odd number");
+}
+}
+}
