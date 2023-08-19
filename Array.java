a985bf9ce9fd515ace8cadc1d1ee35efa82e4e72
Vaishnavi-bele <142570287+Vaishnavi-bele@users.noreply.github.com>
2023-08-19 12:14:43 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        String a="sham";
+        String name[]={"sham","ram","sita","radha"}
+;
+System.out.println (name[3]);
+    }
+}
